2806d306610a68b1b791c77723508c0084a66940
hendrixfan <w.wohanka@gmail.com>
2023-10-01 21:31:27 +0200

    oom

--- lib/friendly_captcha.rb
+++ lib/friendly_captcha.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "friendly_captcha/version"
+require_relative "friendly_captcha/view_helpers"
+require 'dry-configurable'
 
 module FriendlyCaptcha
+  class Error < StandardError; end
   extend Dry::Configurable
 
   setting :secret
--- lib/friendly_captcha/view_helpers.rb
+++ lib/friendly_captcha/view_helpers.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
 module FriendlyCaptcha
   module ViewHelpers
     def friendly_captcha(opts = {})
-      tag(:div, opts)
+      site_key ||= FriendlyCaptcha.config.site_key
+      html = <<-HTML
+        div class="frc-captcha"  data-sitekey="#{site_key}" data-start="auto"></div>
+      HTML
+
+      html
     end
   end
 end
